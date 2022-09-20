ff82d76803c834d7d3c8fd22f72dfbc3473ed1ff
SalmaRohadatulAisy26 <salmarohadatulaisy46@gmail.com>
2022-09-20 14:32:40 +0700

    fits commit

---
+++ website/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from untirta.views import coba
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('untirta/', coba)
+]
